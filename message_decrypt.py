a067439a30f539c6060e4a1ebeb1666ac6a29816
sherwoac <adam.sherwood@gmail.com>
2017-02-06 14:08:50 +0000

    nicer

--- message_decrypt.py
+++ message_decrypt.py
@@ -1,8 +1,12 @@
 import base64
+
+def decrypt_message(encrypted_message, google_user):
+    """ decrypts google foobar end message """
+    encrypted_message_b64 = base64.b64decode(encrypted_message)
+    google_user *= (1 + len(encrypted_message) / len(google_user))
+    return ''.join(chr(ord(a) ^ ord(b)) for a, b in zip(encrypted_message_b64, google_user))
+
+
 # your text goes here
-s1 = 'GkMSGE0QDRYBUE9VREYDEwhPB09JUlAMAAgNAQAKWxZPRUhXSAoXFQEEAEsXT0lSUAoJAg4WFR4J U1JFVR4BDBYEAAgPQhZPSVJQDgwMCAEXCEMWBhFVV1VPQxQKDQJNGA0BVVtPSBYABgMEWgBPRUhX SBwFBwFGQQ5UDgodUE9VREYTCAMPVBU= '
-s2 = base64.b64decode(s1)
-# your username goes here
-s3 = 'adam.sherwood'
-s3 *= (1 + len(s2) / len(s3))
-print ''.join(chr(ord(a) ^ ord(b)) for a, b in zip(s2,s3))
+print decrypt_message('GkMSGE0QDRYBUE9VREYDEwhPB09JUlAMAAgNAQAKWxZPRUhXSAoXFQEEAEsXT0lSUAoJAg4WFR4J U1JFVR4BDBYEAAgPQhZPSVJQDgwMCAEXCEMWBhFVV1VPQxQKDQJNGA0BVVtPSBYABgMEWgBPRUhX SBwFBwFGQQ5UDgodUE9VREYTCAMPVBU= ',
+    'adam.sherwood')
